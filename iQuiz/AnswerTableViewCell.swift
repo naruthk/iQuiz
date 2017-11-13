1e406fb73a76c2ea938274c0018647d374cb3101
Naruth Kongurai <naruth1212@gmail.com>
2017-11-12 23:29:28 -0800

    Questions View Controller implemented

---
+++ iQuiz/AnswerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnswerTableViewCell.swift
+//  iQuiz
+//
+//  Created by Naruth Kongurai on 11/12/17.
+//  Copyright © 2017 Naruth Kongurai. All rights reserved.
+//
+
+import UIKit
+
+class AnswerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ iQuiz/Questions.swift
@@ -0,0 +1,9 @@
+//
+//  Questions.swift
+//  iQuiz
+//
+//  Created by Naruth Kongurai on 11/12/17.
+//  Copyright © 2017 Naruth Kongurai. All rights reserved.
+//
+
+import Foundation
